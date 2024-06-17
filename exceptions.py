62e263289ec1077a9df644b075f3e16a33b91253
Luca Becker <luca.dev@outlook.de>
2024-06-17 15:09:20 +0200

    replaced “ with ‘
    Signed-off-by: Luca Becker <luca.dev@outlook.de>

--- exceptions.py
+++ exceptions.py
@@ -1,27 +1,30 @@
 class PlayerNotFoundException(Exception):
     """Exception raised for errors in the Rating System."""
 
-    def __init__(self, *args:str):
+    def __init__(self, *args: str):
         super().__init__(f"Spieler {', '.join(list(args))} nicht in der Datenbank gefunden.")
 
 
 class PlayerAlreadyExistsException(Exception):
     """Exception raised for errors in the Rating System."""
 
-    def __init__(self, *args:str):
-        super().__init__(f"Spieler {', '.join(list(args))} existier{"en" if len(args) > 1  else "t"} bereits in der Datenbank.")
-        
+    def __init__(self, *args: str):
+        super().__init__(f"Spieler {', '.join(list(args))} existier{'en' if len(args) > 1  else 't'} bereits in der Datenbank.")
+
+
 class PlayerNotInRatingException(Exception):
     """Exception raised for errors in the Rating System."""
 
-    def __init__(self, *args:str):
+    def __init__(self, *args: str):
         super().__init__(f"Spieler {', '.join(list(args))} nicht im Rating gefunden.")
-        
+
+
 class PlayerAlreadyInRatingException(Exception):
     """Exception raised for errors in the Rating System."""
 
-    def __init__(self, *args:str):
-        super().__init__(f"Spieler {', '.join(list(args))} existier{"en" if len(args) > 1  else "t"} bereits im Rating.")
+    def __init__(self, *args: str):
+        super().__init__(f"Spieler {', '.join(list(args))} existier{'en' if len(args) > 1  else 't'} bereits im Rating.")
+
 
 class GameNotFoundException(Exception):
     """Exception raised for errors in the Rating System."""
@@ -29,6 +32,8 @@ class GameNotFoundException(Exception):
     def __init__(self, game_id: int):
         super().__init__(f"Spiel {game_id} nicht in der Datenbank gefunden.")
         self.game_id = game_id
+
+
 class GameTypeNotSupportedException(Exception):
     """Exception raised for errors in the Rating System."""
 
@@ -42,4 +47,3 @@ class PlayerNotInGameException(Exception):
 
     def __init__(self):
         super().__init__(f"Du kannst nur Spiele hinzufügen, in denen du mitspielst.")
-
